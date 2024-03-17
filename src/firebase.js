2a0afe8150268a678a4aa6386b8a7b50b0d72b13
Devadharshini-Nagarajan <nagarajan.de@northeastern.edu>
2024-03-17 14:53:36 -0400

    feat: env added

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDdrUn1McsSuFT5jEiM-xhIBiyCT3Hfuxk",
-    authDomain: "disney-clone-9bb71.firebaseapp.com",
-    projectId: "disney-clone-9bb71",
-    storageBucket: "disney-clone-9bb71.appspot.com",
-    messagingSenderId: "971104849329",
-    appId: "1:971104849329:web:75f65cbf215cc71de34190",
-    measurementId: "G-PRRPBJMRNB"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+};
 
 
 const firebaseApp =  firebase.initializeApp(firebaseConfig);
